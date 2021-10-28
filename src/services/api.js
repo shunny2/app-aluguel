faa41749bb2844fc3287745fbc0657a31c1d4817
Alexander Davis <72872854+shunny2@users.noreply.github.com>
2021-10-28 16:47:34 -0300

    none

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://ac83-152-249-4-218.ngrok.io/api',
+    baseURL: 'https://d9c5-2804-431-d77e-9aa2-75ea-d6f3-4fec-f7e1.ngrok.io/api',
 });
 
 export default instance;
